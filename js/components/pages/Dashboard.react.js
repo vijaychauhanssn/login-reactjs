074cb70ca4a44ade5ea0860fb58abc1626362bc3
vijaychauhanssn <vijaychauhanssn@gmail.com>
2017-08-15 15:37:26 +0530

    first commit

--- js/components/pages/Dashboard.react.js
+++ js/components/pages/Dashboard.react.js
@@ -15,9 +15,9 @@ class Dashboard extends Component {
       <article>
         <section className="text-section">
           <h1>Dashboard</h1>
-          <p>Welcome, you are logged in! To have a look at the code behind this application, go to <a href="https://github.com/mxstbr/login-flow">Github</a>. To run it locally:</p>
+          <p>Welcome, you are logged in! To have a look at the code behind this application, go to <a href="https://github.com/vijaychauhanssn/login-reactjs">Github</a>. To run it locally:</p>
           <ol>
-            <li><p>Clone the repo using <code>git clone git@github.com:mxstbr/login-flow</code></p></li>
+            <li><p>Clone the repo using <code>git clone git@github.com:vijaychauhanssn/login-reactjs</code></p></li>
             <li><p>Run <code>npm install</code> to install the dependencies.</p></li>
             <li><p>Run <code>npm start</code> to start the local web server</p></li>
             <li><p>Go to <a href="http://localhost:3000"><code>http://localhost:3000</code></a> and you should see it running!</p></li>
